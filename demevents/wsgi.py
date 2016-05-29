964e3168b5fc09e084d32e7a17afc31344d61e95
bahoo <jon.c.culver@gmail.com>
2016-05-28 22:48:44 -0700

    off by 1.

--- demevents/wsgi.py
+++ demevents/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demevents.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application
+application = DjangoWhiteNoise(application)

ed53e945a6a326b1845d600942f845d42ad1131a
bahoo <jon.c.culver@gmail.com>
2016-05-30 19:08:22 -0700

    A little better.

--- core/views.py
+++ core/views.py
@@ -19,7 +19,7 @@ class MapView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MapView, self).get_context_data(**kwargs)
 
-        initial_data = {'address': 'Seattle, WA', 'distance': '15', 'days': '30', 'event_types': []}
+        initial_data = {'address': 'Seattle, WA', 'distance': '15', 'days': '30', 'event_types': list(k for (k, v) in Event.EVENT_TYPE_CHOICES)}
         search_form = SearchForm(self.request.GET or initial_data)
         address = search_form.data.get('address', initial_data.get('address'))
         distance = int(search_form.data.get('distance', initial_data.get('distance')))
@@ -30,8 +30,10 @@ class MapView(TemplateView):
         point = GEOSGeometry('POINT(%(lng)s %(lat)s)' % {'lng': location['lng'], 'lat': location['lat']}, srid=4326)
 
         type_filter = Q()
-        for event_type in self.request.GET.getlist('event_types', initial_data.get('event_types', [])):
-            type_filter = type_filter | Q(event_type=event_type)
+        event_types = self.request.GET.getlist('event_types', initial_data.get('event_types', []))
+        if event_types:
+            for event_type in event_types:
+                type_filter = type_filter | Q(event_type=event_type)
 
         events = Event.objects.filter(type_filter).filter(venue__point__distance_lte=(point, D(mi=distance))).select_related('venue', 'host').filter_by_date(days=days).annotate(distance=Distance('venue__point', point)).order_by('distance')
 

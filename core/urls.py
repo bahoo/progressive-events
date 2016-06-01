5c2399068c29e8ee5be72149f03ce91e9159ebbc
bahoo <jon.c.culver@gmail.com>
2016-05-31 22:11:22 -0700

    Yeah dawg.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
+from django.views.i18n import javascript_catalog
 from .views import AddView, MapView, WhyView
+
+js_info_dict = {
+    'packages': ('recurrence', ),
+}
 
 urlpatterns = [
     url(r'^$', MapView.as_view(), name='index'),
     url(r'^why$', WhyView.as_view(), name='why'),
-    url(r'^add$', AddView.as_view(), name='add')
-
+    url(r'^add$', AddView.as_view(), name='add'),
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict)
 ]
